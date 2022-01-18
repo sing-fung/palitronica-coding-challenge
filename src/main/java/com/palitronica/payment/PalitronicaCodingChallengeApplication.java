a247410a27b43ad090bec2040cea0e040ab6e61e
sing-fung <1587898817@qq.com>
2022-01-18 18:11:34 -0500

    Initial commit

---
+++ src/main/java/com/palitronica/payment/PalitronicaCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.palitronica.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalitronicaCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalitronicaCodingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/palitronica/payment/PalitronicaCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palitronica.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalitronicaCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
