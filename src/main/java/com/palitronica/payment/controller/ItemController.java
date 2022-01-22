586252529176334ab8e4a06f5f8961e042c7e840
sing-fung <1587898817@qq.com>
2022-01-22 14:45:31 -0500

    add queryAll()

--- src/main/java/com/palitronica/payment/controller/ItemController.java
+++ src/main/java/com/palitronica/payment/controller/ItemController.java
@@ -9,10 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 /**
  * @author sing-fung
@@ -39,5 +37,11 @@ public class ItemController
 
         itemService.save(dto);
         return ResponseEntity.status(HttpStatus.OK).body(Result.success());
+    }
+
+    @GetMapping
+    public List<ItemDTO> queryAll()
+    {
+        return itemService.queryAll();
     }
 }
